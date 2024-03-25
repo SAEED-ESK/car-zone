74f56db09bd0632db16d6a07535da2c368354a19
Saeed Eskandari <eskandary.saeed336@gmail.com>
2024-03-26 01:51:55 +0330

    first commit

---
+++ env_site/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\eskan\onedrive\desktop\projects\py-projects\carzone\env_site\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
